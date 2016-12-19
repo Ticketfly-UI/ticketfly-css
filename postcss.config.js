759564807375534f8358f730ad6fc9c10706c470
BrianSipple <bsipple57@gmail.com>
2016-12-19 03:08:27 -0800

    proper integration of stylelint into build scripts

--- postcss.config.js
+++ postcss.config.js
@@ -6,29 +6,30 @@
  * Properties names correspond to the `postcss-cli` properties
  * accepted by the PostCSS CLI API -- with the addition of
  * plugin names for plugin settings.
- *
- * @see: https://github.com/postcss/postcss-cli
+ * (@see: https://github.com/postcss/postcss-cli)
  */
 module.exports = {
   input: 'src/index.css',
   output: 'dist/ticketfly-css.css',
 
+  // ⚠️ Order matters! PostCSS will run plugins in the order listed.
   use: [
+    'stylelint',
     'postcss-import',
+    'autoprefixer',
+    'cssstats',
     'postcss-custom-media',
     'postcss-custom-properties',
     'postcss-calc',
-    'cssstats',
     'postcss-discard-comments',
     'postcss-remove-root',
-    'autoprefixer',
     'postcss-reporter'
   ],
 
   stylelint: {
     configFile: './stylelint.config.js',
     configOverrides: {
-      ignoreFiles: '**/normalize/index.css'
+      ignoreFiles: '**/ticketfly-css-normalize/**'
     },
     plugins: [
       'stylelint'
