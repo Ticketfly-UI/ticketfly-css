dabbf0ce12ff3933d0d84484bdcae772c8a4d6d3
BrianSipple <bsipple57@gmail.com>
2016-12-29 01:18:41 -0800

    deleting folder for `ticketfly-css-reset-garnishes`

--- modules/garnishes/resets/CHANGELOG.md
+++
@@ -1,6 +0,0 @@
-# 0.0.1 (December 28, 2016)
-
-- **[ADDITION]**: Initial project setup
-  + Initial ruleset
-  + Initial demo page
-
--- modules/garnishes/resets/postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  input: 'index.css',
-  output: 'build/ticketfly-css-reset-garnishes.css',
-
-  use: [
-    'postcss-cssnext'
-  ]
-};
