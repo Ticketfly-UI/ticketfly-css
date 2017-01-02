11d46c599967a7e5e4e64ca146e2445674c1fa4f
BrianSipple <bsipple57@gmail.com>
2017-01-01 18:53:39 -0800

    setup postcss compilation of index.css by extending addon hooks

---
+++ blueprints/ticketfly-css/index.js
@@ -0,0 +1,30 @@
+/* global module */
+
+const postCSSCompilingPackages = [
+  { name: 'broccoli-funnel', target: '^1.1.0' },
+  { name: 'broccoli-merge-trees',target: '^1.2.1' },
+  { name: 'broccoli-postcss-single', target: '^1.2.0' },
+  { name: 'postcss-cssnext', target: '^2.9.0' },
+  { name: 'postcss-import', target: '^9.0.0' }
+];
+
+module.exports = {
+  description: 'install ticketfly-css into an Ember CLI project',
+
+  normalizeEntityName: function() {},
+
+  afterInstall: function(options) {
+    return this.addPackagesToProject(postCSSCompilingPackages);
+  }
+
+  // locals: function(options) {
+  //   // Return custom template variables here.
+  //   return {
+  //     foo: options.entity.options.foo
+  //   };
+  // }
+
+  // afterInstall: function(options) {
+  //   // Perform extra work here.
+  // }
+};
--- tests/.eslintrc.js
+++ tests/.eslintrc.js
@@ -1,5 +1,19 @@
 module.exports = {
   env: {
     'embertest': true
+  },
+  rules: {
+    'lines-around-comment': ['error', {
+      beforeBlockComment: true,
+      afterBlockComment: false,
+      beforeLineComment: false,
+      afterLineComment: false,
+      allowBlockStart: true,
+      allowBlockEnd: false,
+      allowObjectStart: true,
+      allowObjectEnd: false,
+      allowArrayStart: true,
+      allowArrayEnd: false
+    }]
   }
 };
