0a1f979b8cbd91de6b73beb70b1d5d3f523ae5d5
BrianSipple <bsipple57@gmail.com>
2017-01-27 18:33:20 -0800

    use package.json instead of blueprint install for index.js dependencies

--- blueprints/ticketfly-css/index.js
+++ blueprints/ticketfly-css/index.js
@@ -1,21 +1,8 @@
 /* global module */
-
-const postCSSCompilingPackages = [
-  { name: 'broccoli-funnel', target: '^1.1.0' },
-  { name: 'broccoli-merge-trees',target: '^1.2.1' },
-  { name: 'broccoli-postcss-single', target: '^1.2.0' },
-  { name: 'postcss-cssnext', target: '^2.9.0' },
-  { name: 'postcss-import', target: '^9.0.0' }
-];
-
 module.exports = {
   description: 'install ticketfly-css into an Ember CLI project',
 
-  normalizeEntityName: function() {},
-
-  beforeInstall: function(options) {
-    return this.addPackagesToProject(postCSSCompilingPackages);
-  }
+  normalizeEntityName: function() {}
 
   // locals: function(options) {
   //   // Return custom template variables here.
